c4a8cd32293ba3159fc54870323320fed69870d9
Bright Atsighi <brytebee@gmail.com>
2024-01-24 19:47:01 +0100

    Added a template for email confirmation

---
+++ src/app/preview/page.tsx
@@ -0,0 +1,20 @@
+import ConfirmEmail from "@/components/Confirm";
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <div className="bg-white w-full h-screen">
+      <ConfirmEmail />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Non inventore
+      explicabo possimus corrupti nobis omnis tenetur, perspiciatis accusamus
+      impedit iure praesentium molestias mollitia enim cum soluta tempora
+      blanditiis iste et alias voluptatum vel earum libero asperiores! Magnam,
+      nihil quidem rerum accusamus earum beatae placeat iste, officiis officia
+      molestiae suscipit optio!
+    </div>
+  );
+};
+
+export default page;
---
+++ src/app/v1/confirmation/route.ts
@@ -0,0 +1,32 @@
+import ConfirmEmail from "@/components/Confirm";
+import EmailTemplate from "@/components/EmailTemplate";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+const sendMail = async (emailData: any) => {
+  const { from, to, subject, firstName, product, code } = emailData;
+  const contacts = JSON.parse(to.replace(/'/g, '"'));
+
+  try {
+    const res = await resend.emails.send({
+      from: `${product} <${from}>`,
+      to: contacts,
+      subject,
+      react: ConfirmEmail({
+        code,
+        firstName,
+        product,
+      }),
+    });
+
+    return Response.json({ message: "success", res });
+  } catch (error) {
+    return Response.json({ message: "failed", error });
+  }
+};
+
+export async function POST(req: any) {
+  const data = await req.json();
+  return await sendMail(data);
+}
