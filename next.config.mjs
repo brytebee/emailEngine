4ecfb18a2fca213dce09a012013ce7605f91a3b8
Bright Atsighi <brytebee@gmail.com>
2024-03-16 16:12:21 +0100

    Updated next config to solve CORS

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Allow requests from all origins (be cautious in production)
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,PUT,DELETE,OPTIONS", // Allowed methods
+          },
+          // Add other CORS headers if needed (e.g., Allow-Credentials)
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
