d77ceb95c5d806b0b85bd99d5f67ebe035df2e39
Bright Atsighi <brytebee@gmail.com>
2024-03-16 17:06:31 +0100

    Removed routes from the api DIR to observe

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/api/(.*)",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*", // Allow requests from all origins (be cautious in production)
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,POST,PUT,DELETE,OPTIONS", // Allowed methods
-          },
-          // Add other CORS headers if needed (e.g., Allow-Credentials)
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
